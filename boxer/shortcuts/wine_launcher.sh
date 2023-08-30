4213affff31725353e3e4fb5c6ceb7e6e11ba0f7
neofeo <salva.liebana77@gmail.com>
2023-08-30 16:48:32 -0300

    revert wine launcher

--- boxer/shortcuts/wine_launcher.sh
+++ boxer/shortcuts/wine_launcher.sh
@@ -28,39 +28,10 @@ fi
 
 EXEC_DIR=$(dirname "$1")
 
-# Launch Wine with the chosen option and monitor loading
 if [ "$selected_choice" == "Fullscreen" ]; then
     cd "$EXEC_DIR"
-    wine "$1" &
+    wine "$1"
 elif [ "$selected_choice" == "Virtual Desktop" ]; then
     cd "$EXEC_DIR"
-    wine explorer /desktop=1024x768 "$1" &
-fi
-
-# Check if wine is running
-is_wine_running() {
-    pgrep wine >/dev/null
-}
-
-# Display Wine loading message using Zenity
-loading_popup=$(zenity --info --text="Wine is loading. Please wait..." --title="Wine Loading" --timeout 5 --width=250 --height=100)
-
-# Start a loop to monitor Wine loading
-while is_wine_running; do
-    # Check if a Wine window has been created
-    wine_window_id=$(xprop -root | grep -i '_NET_ACTIVE_WINDOW(WINDOW)' | awk '{print $NF}')
-    
-    if [ -n "$wine_window_id" ]; then
-        echo "Wine program has opened a window."
-        break
-    else
-        echo "Wine program is loading..."
-    fi
-    
-    sleep 1  # Adjust the sleep duration if needed
-done
-
-# Close the loading pop-up if it's still open
-if [ -n "$loading_popup" ]; then
-    kill "$loading_popup" >/dev/null 2>&1
+    wine explorer /desktop=1024x768 "$1"
 fi
