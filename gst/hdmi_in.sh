e78b4fcdde0d2981d4df05135d9fe92b3c9fc9da
neofeo <salva.liebana77@gmail.com>
2023-09-16 16:50:24 -0300

    adding gst scripts 3

--- gst/hdmi_in.sh
+++ gst/hdmi_in.sh
@@ -33,7 +33,7 @@ toggle_fullscreen() {
 }
 
 # Launch the GStreamer pipeline
-gst-launch-1.0 -e alsasrc device=hw:CARD=rockchiphdmiin,DEV=0 ! audioconvert ! audioresample ! audio/x-raw,rate=48000 ! queue max-size-buffers=1000 max-size-bytes=0 max-size-time=1000000000 ! pulsesink device="$current_pulse_sink" sync=false async=false
+gst-launch-1.0 -e alsasrc device=hw:CARD=rockchiphdmiin,DEV=0 ! audioconvert ! queue max-size-buffers=1000 max-size-bytes=0 max-size-time=1000000000 ! pulsesink device="$current_pulse_sink" sync=false async=false
 audio_pid="$!"
 
 
